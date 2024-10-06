f369e4603392c67ffdbc7a718af5f20c3a959f63
ediallo <elhhabdouldiallo@gmail.com>
2024-10-06 21:25:20 +0100

    create c14 program to calculate age based on pct

---
+++ control_flow/c_14.py
@@ -0,0 +1,20 @@
+
+import math 
+
+K = -8266.64
+
+def main():
+    calculate_age_single_sample()
+
+
+
+def calculate_age_single_sample():
+    pct_left = float(input(" % of natural c14: "))
+    age = K * math.log(pct_left / 100)
+
+    print("sample is", str(age), "years old")
+
+
+
+if __name__ == '__main__':
+    main()

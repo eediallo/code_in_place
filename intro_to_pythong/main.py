549074b4816f26087a9e9e7ad1f76d8f06f5e7ec
ediallo <elhhabdouldiallo@gmail.com>
2024-10-06 13:04:16 +0100

    prints hello world in the console

---
+++ intro_to_pythong/main.py
@@ -0,0 +1,15 @@
+
+"""
+This is an example of the classic "Hello world" 
+program, written in standard python! 
+"""
+
+# The main function is the entry point of the program
+def main():
+    # This print Hello World in the console
+    print("Hello World")
+
+
+#Run the main function when this file is executed
+if __name__ == '__main__':
+    main()

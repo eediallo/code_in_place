f6b1badfa468555819c1813b4f9778e07d259ae0
ediallo <elhhabdouldiallo@gmail.com>
2024-10-09 18:00:16 +0100

    prints a text 100 times

---
+++ control_flow/print_100_times.py
@@ -0,0 +1,16 @@
+
+
+
+def main():
+    print_python_socks_100_times()
+
+
+
+def print_python_socks_100_times():
+    for i in range(3):
+        print(i, ": Python rocks socks!")
+
+
+
+if __name__ == '__main__':
+    main()
